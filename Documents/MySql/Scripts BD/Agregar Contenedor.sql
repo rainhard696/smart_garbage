9732bd35b01fa1b99c5224b467b1492478c906fb
JYMBRUNO <71528986+JYMBRUNO@users.noreply.github.com>
2020-10-16 16:58:33 -0500

    Create Agregar Contenedor.sql

---
+++ Documents/MySql/Scripts BD/Agregar Contenedor.sql
@@ -0,0 +1,11 @@
+/* Script para agregar contenedor. Reemplazar los datos de values */
+
+INSERT INTO `smartgarbage`.`contenedor`
+(
+`Capacidad`,
+`Latitud`,
+`Longitud`,
+`Estado`,
+`IsAssigned`)
+VALUES
+(50,-12.048236, -75.227899,'Lleno','NO');
